a6a267123c55999d7ef1fb5f95fb1ad6fc470f34
juanclopezluna <juan.c.lopezluna@gmail.com>
2020-11-07 16:20:49 -0500

    test for heroku

--- public/script.js
+++ public/script.js
@@ -1,56 +1,53 @@
-const arr1 = []; 
-
-
-fetch("/api", {
-  method: 'post',
+ fetch('/api', {
+  method: 'POST',
   headers: {
     'Content-Type': 'application/json', 
   },
 }).then(blob => blob.json())
   .then(data => arr1.push(...data));
 
+const searchInput = document.querySelector('.userInput');
+const suggestions = document.querySelector('.suggestions');
+searchInput.addEventListener('keyup',displayMatches);
 
-
+const arr1 = [];
 
 function findMatches(wordToMatch, arr1) {  
-    return arr1.filter((resturant) => {
-
+    return arr1.filter((restaurant) => {
         const regex = new RegExp(wordToMatch, 'gi');
-        return resturant.name.match(regex) || resturant.category.match(regex) || resturant.city.match(regex) || resturant.state.match(regex) || resteraunt.address_line_1.match(regex)
+        return restaurant.name.match(regex) || restaurant.category.match(regex); //|| restaurant.city.match(regex) || restaurant.state.match(regex) || restaurant.address_line_1.match(regex);
 
     });
 }
 
 function displayMatches() { 
     const matchArray = findmatches(this.value, arr1);
-    const html = matchArray.map(resteraunt => { 
-      const regex = new RegExp(this.value,'gi');
-      const name = arr1.name.replace(regex, `<span class="h1">'${this.
-      value}</span>`)
-      const category = arr1.category.replace(regex, `<span class="h1">'${this.
-      value}</span>`)
-      const city = arr1.city.replace(regex, `<span class="h1">'${this.
-      value}</span>`)
-      const state = arr1.state.replace(regex, `<span class="h1">'${this.
-      value}</span>`)
-      const address = arr1.address_line_1.replace(regex, `<span class="h1">'${this.
-      value}</span>`)
+    const html = matchArray.map(restaurant => { 
+     // const regex = new RegExp(this.value,'gi');
+     // const name = arr1.name.replace(regex, `<span class="name">'${this.
+     // value}</span>`)
+     // const category = arr1.category.replace(regex, `<span class="categrory">'${this.
+     // value}</span>`)
+     // const city = arr1.city.replace(regex, `<span class="h1">'${this.
+     // value}</span>`)
+     // const state = arr1.state.replace(regex, `<span class="h1">'${this.
+     // value}</span>`)
+     // const address = arr1.address_line_1.replace(regex, `<span class="h1">'${this.
+     // value}</span>`)
 
         return `
         <li>
-            <span class="name">${resteraunt.name} </span>
-            <span class="category">${resteraunt.category}</span>
-            <span class="city">${resteraunt.city}</span>
-            <span class="state">${resteraunt.state}</span>
-            <span class="address">${resteraunt.address_line_1}</span>
+            <span class="name">${restaurant.name} </span>
+            <span class="category">${restaurant.category}</span>
         </li>
         `;
     }).json('');
     suggestions.innerHTML = html;
 }
 
-const searchInput = document.querySelector('.textInput');
-const suggestions = document.querySelector('.suggestions');
 
-searchInput.addEventListener('input',displayMatches);
-searchInput.addEventListener('keyup',displayMatches);
+
+
+// <span class="city">${restaurant.city}</span>
+// <span class="state">${restaurant.state}</span>
+// <span class="address">${restaurant.address_line_1}</span>
--- server.js
+++ server.js
@@ -22,17 +22,17 @@ app.use((req, res, next) => {
 });
 
 app.route('/api')
-  .get(async(req, res) => {
-    console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
-  })
+  .get(async function (req, res) {
+      console.log('GET request detected');
+      res.send(`Lab 5 for ${process.env.NAME}`);
+    })
   
   .post(async(req, res) => {
 
     console.log('POST request detected');
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
-    console.log('Fetch request data', data);
+    console.log('Fetch request data', req.body);
     res.json(json);
   });
 
