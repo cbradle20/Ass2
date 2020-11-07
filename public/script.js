6e5ef1c5bd3c0cb710d17fd07d69c4982f4dcb3e
bcohn13 <bcohn13@yahoo.com>
2020-11-06 20:25:17 -0500

    Changed so we can add to the array and added span tags for each element

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 const arr1 = []; // need to put the api stuff into this array somehow
 
+  arr1.push(jsonFromServer);
+  console.log(arr1);
 
 
 function findMatches(wordToMatch, arr1) {   // this finds and filters things in such array
@@ -7,7 +9,7 @@ function findMatches(wordToMatch, arr1) {   // this finds and filters things in 
 
         const regex = new RegExp(wordToMatch, 'gi');
         return resturant.name.match(regex) || resturant.category.match(regex) 
-        || resturant.city.match(regex) || resturant.state.match(regex)
+        || resturant.city.match(regex) || resturant.state.match(regex) || resteraunt.address_line_1.match(regex)
 
     });
 }
@@ -17,14 +19,16 @@ function displayMatches() {    //displays it
     const html = matchArray.map(resturant => { //finish the list thing
         return `
         <li>
-            <span class =    
+            <span class="name">${resteraunt.name} </span>
+            <span class="category">${resteraunt.category}</span>
+            <span class="city">${resteraunt.city}</span>
+            <span class="state">${resteraunt.state}</span>
+            <span class="address">${resteraunt.address_line_1}</span>
+        </li>
         `;
     }).json('');
     suggestions.innerHTML = html;
 }
-
-
-
 
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
@@ -36,17 +40,18 @@ searchInput.addEventListener('keyup',displayMatches);
 //stopped following the video at the 12 min mark
 
 
+function runThisWithResultsFromServer(jsonFromServer) {
+  console.log('jsonFromServer', jsonFromServer);
+  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
+  // Process your restaurants list
+  // Make a configuration object for your chart
+  // Instantiate your chart
+  const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
+  const options = makeYourOptionsObject(reorganizedData);
+  
+}
 
-
-
-
-
-
-
-
-
-
-document.body.addEventListener('submit', async (e) => { // I got this from lab 7,not sure if it is needed
+document.body.addEventListener('keyup', async (e) => { // I got this from lab 7,not sure if it is needed
     e.preventDefault(); 
     const form = $(e.target).serializeArray();
     fetch('/api', {
